74d946c0a9206ce8eced6fb56f554173b2f17372
CAROLINA GIRALDO LOPEZ <carolinagiraldo240127@correo.itm.edu.co>
2023-05-15 17:43:12 -0500

    Implementando el INotifyPropertyChanged automáticamente

--- MAUITest/MAUITest/App.xaml.cs
+++ MAUITest/MAUITest/App.xaml.cs
@@ -9,6 +9,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-        MainPage = new NavigationPage(new CommandsView());
+        MainPage = new NavigationPage(new DemoAutoPropertyChangedView());
     }
 }
